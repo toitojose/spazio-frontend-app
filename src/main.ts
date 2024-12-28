652cabac63788262a6238747a0efefd6e3382eee
Antonio Cordova <acordova@creditu.com>
2024-12-27 20:45:25 -0500

    Instala primevue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
 import 'tailwindcss/tailwind.css';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 
-app.use(router); // Usar el router
+app.use(router);
+app.use(createPinia());
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
 app.mount('#app');
