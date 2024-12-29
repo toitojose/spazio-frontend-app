00bbcea7d1dd0de27714279cb65ef5b349c3c929
Antonio Cordova <acordova@creditu.com>
2024-12-29 14:46:02 -0500

    Se define Aura como tema del frontend

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,30 @@ import PrimeVue from 'primevue/config';
 import 'tailwindcss/tailwind.css';
 import 'public/spazio.css';
 import 'primeicons/primeicons.css';
-import Aura from '@primevue/themes/aura';
 import messages from '@/locales';
 import { createI18n } from 'vue-i18n';
+import Aura from '@primevue/themes/aura';
+import { createHead } from '@vueuse/head';
 
 const i18n = createI18n({
   locale: 'es',
   fallbackLocale: 'en',
   messages,
 });
+const headHtml = createHead();
 
 const app = createApp(App);
-
+app.use(headHtml);
 app.use(router);
 app.use(createPinia());
 app.use(i18n);
 app.use(PrimeVue, {
+  ripple: true,
   theme: {
     preset: Aura,
     options: {
-      prefix: 'pv',
-      darkModeSelector: 'system',
-      cssLayer: false, // Desactiva la capa CSS si no es necesaria
+      cssLayer: false,
+      darkModeSelector: 'none',
     },
   },
 });
