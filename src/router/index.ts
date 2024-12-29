e16cad808a58ca41a27c520e027fa1e6853e8253
Antonio Cordova <acordova@creditu.com>
2024-12-29 16:24:40 -0500

    Se actualiza para control dinámico el título y las meta tags. Se agrega meta en la ruta principal

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import AuthRoutes from '@/router/public/auth-routes.ts';
+import AuthRoutes from '@/router/public/auth-routes';
+import { i18n } from '@/locales/i18n';
 
 // Rutas públicas
 const publicRoutes = [
   {
     path: '/',
-    name: 'main',
+    name: 'home',
     component: () => import('@/ui/views/public/Home.vue'),
+    meta: {
+      title: 'default.title',
+      metaTitle: 'default.metaTitle',
+      metaDescription: 'default.metaDescription',
+    },
   },
 ];
 
+// Consolidar rutas
 const routes = [...publicRoutes, ...AuthRoutes];
 
+// Crear instancia del router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+// Actualizar dinámicamente el título y las meta tags
+router.afterEach((to) => {
+  const metaTitle = to.meta?.metaTitle ? (i18n.global.t(to.meta.metaTitle as string) as string) : '';
+  const metaDescription = to.meta?.metaDescription ? (i18n.global.t(to.meta.metaDescription as string) as string) : '';
+
+  // Actualizar título de la página
+  document.title = metaTitle;
+
+  // Actualizar las meta tags
+  const metaTitleTag = document.querySelector('meta[name="title"]') || createMetaTag('title');
+  const metaDescriptionTag = document.querySelector('meta[name="description"]') || createMetaTag('description');
+
+  metaTitleTag.setAttribute('content', metaTitle);
+  metaDescriptionTag.setAttribute('content', metaDescription);
+});
+
+// Función auxiliar para crear meta tags si no existen
+function createMetaTag(name: string): HTMLMetaElement {
+  const metaTag = document.createElement('meta');
+  metaTag.setAttribute('name', name);
+  document.head.appendChild(metaTag);
+  return metaTag;
+}
 
 export default router;
