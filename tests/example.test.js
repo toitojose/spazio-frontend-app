af430847c42552c850558fbf0bd5408b15dab84b
toitojose <cordovantonio@gmail.com>
2025-02-18 22:19:52 -0500

    Refactoriza interfaces relacionadas con el usuario y servicios, ademas agregar validacion del formulario del propietario.
    implementa seleccion de escenarios para inquilinos

---
+++ tests/example.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var vitest_1 = require("vitest");
+(0, vitest_1.describe)('Ejemplo de test', function () {
+    (0, vitest_1.it)('debería ser true', function () {
+        (0, vitest_1.expect)(true).toBe(true);
+    });
+});
