5956727868d0c78e48d0ca884f2b8a3903022b56
sebastian alban <sebassarrivera@hotmail.com>
2025-04-09 16:41:18 -0500

    Asociados productos

--- src/services/imagesProduct-services.ts
+++ src/services/imagesProduct-services.ts
@@ -10,16 +10,18 @@ export class ImageService {
 
   async getImages(productId: number): Promise<ImageResult> {
     try {
-      const response = await this.authBackendClient.get<ImageResult>(
-        `http://localhost:7000/products/${productId}/photos`,
-      );
-      return response.data;
+      const response = await this.authBackendClient.get<string[]>(`http://localhost:7000/products/${productId}/photos`);
+      return {
+        result: true,
+        message: 'Success',
+        data: response.data,
+      };
     } catch (error: unknown) {
       if (error instanceof Error && 'response' in error) {
         const axiosError = error as any; // Cast a 'any' o un tipo más específico si usas Axios
         return {
           result: false,
-          message: 'Error al obtener el producto',
+          message: 'Error al obtener las imagenes',
           error: {
             statusCode: axiosError.response?.status || 500,
             key: axiosError.response?.statusText || 'INTERNAL_SERVER_ERROR',
@@ -39,5 +41,57 @@ export class ImageService {
         data: undefined,
       };
     }
+  }
+
+  async uploadImages(productId: number, files: File[]): Promise<ImageResult> {
+    const uploadedImages: any[] = [];
+
+    try {
+      for (const file of files) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await this.authBackendClient.post(`http://localhost:7000/upload/${productId}`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        uploadedImages.push(response.data);
+      }
+
+      return {
+        result: true,
+        message: 'Todas las imágenes fueron subidas exitosamente',
+        data: uploadedImages,
+      };
+    } catch (error: unknown) {
+      return this.handleError(error, 'Error subiendo una o más imágenes');
+    }
+  }
+
+  private handleError(error: unknown, defaultMessage: string): ImageResult {
+    if (error instanceof Error && 'response' in error) {
+      const axiosError = error as any;
+      return {
+        result: false,
+        message: defaultMessage,
+        error: {
+          statusCode: axiosError.response?.status || 500,
+          key: axiosError.response?.statusText || 'INTERNAL_SERVER_ERROR',
+        },
+        data: undefined,
+      };
+    }
+
+    return {
+      result: false,
+      message: defaultMessage,
+      error: {
+        statusCode: 500,
+        key: 'UNKNOWN_ERROR',
+      },
+      data: undefined,
+    };
   }
 }
